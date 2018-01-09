d81c6184354bd4ebd8bc7c64f23fba184ac0852f
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2018-01-09 14:18:47 +0100

    context path /api, version endpoint, version generation using git-commit-id-plugin

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # CI build script
 set -e
-cd `dirname $0`
-./mvnw clean
-mkdir -p target/classes
-git describe > target/classes/VERSION
-./mvnw verify
+./mvnw clean verify
+git describe > target/version
--- src/main/java/com/example/webappboilerplate/Application.java
+++ src/main/java/com/example/webappboilerplate/Application.java
@@ -2,11 +2,22 @@ package com.example.webappboilerplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Bean /* see http://www.baeldung.com/spring-git-information */
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer gitInfo = new PropertySourcesPlaceholderConfigurer();
+        gitInfo.setLocation(new ClassPathResource("git.properties"));
+        gitInfo.setIgnoreResourceNotFound(true);
+        gitInfo.setIgnoreUnresolvablePlaceholders(true);
+        return gitInfo;
+    }
 }
